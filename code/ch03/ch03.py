c802f804133811375a80843ad4fb8b8c1209628c
yosho <44283410+yosho@users.noreply.github.com>
2019-07-01 19:29:00 +0900

    Update

--- code/ch03/ch03.py
+++ code/ch03/ch03.py
@@ -515,23 +515,27 @@ svm = SGDClassifier(loss='hinge', n_iter=1000)
 
 
 
-
+#乱数シードを指定
 np.random.seed(1)
+#標準正規分布に従う乱数で200行×2列の行列を生成
 X_xor = np.random.randn(200, 2)
+#２つの引数に対して排他的論理和を実行
 y_xor = np.logical_xor(X_xor[:, 0] > 0,
                        X_xor[:, 1] > 0)
+#排他的論理和の値が真の場合は1，偽の場合は-1を割り当てる
 y_xor = np.where(y_xor, 1, -1)
-
+#ラベル1を青の×でプロット
 plt.scatter(X_xor[y_xor == 1, 0],
             X_xor[y_xor == 1, 1],
             c='b', marker='x',
             label='1')
+#ラベル-1を赤の四角でプロット
 plt.scatter(X_xor[y_xor == -1, 0],
             X_xor[y_xor == -1, 1],
             c='r',
             marker='s',
             label='-1')
-
+#軸の範囲を設定
 plt.xlim([-3, 3])
 plt.ylim([-3, 3])
 plt.legend(loc='best')
